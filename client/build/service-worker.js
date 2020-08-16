abf47e68770d0fad45ee2314535cf710a1ed0a33
William Feng <williamfeng1729@gmail.com>
2020-08-16 16:17:47 -0700

    npm run build instead of just npm build

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e71710041d78fc571be775fe1991271.js"
+  "/precache-manifest.556e45cb14fefd8514263eaad4d64c21.js"
 );
 
 self.addEventListener('message', (event) => {
